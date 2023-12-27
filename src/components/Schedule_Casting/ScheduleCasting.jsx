33ddb0400f9bba6e21fbea686aab4db8efc6c7a0
nirbhaymaitra07 <nirbhay.maitra@gaiansolutions.com>
2023-12-27 18:48:49 +0530

    Minor changes in Schedule Casting

--- src/components/Schedule_Casting/ScheduleCasting.jsx
+++ src/components/Schedule_Casting/ScheduleCasting.jsx
@@ -12,10 +12,21 @@ import Remove from "./images/Remove.svg";
 import CalendarBody from "./images/Calendar-body.svg";
 import LineBody from "./images/Line-body.svg";
 import Add from "./images/Add.svg";
-import Frame from "./images/Frame.svg"
+import Frame from "./images/Frame.svg";
 
 const ScheduleCasting = () => {
   const [addTimeSlot, setAddTimeSlot] = useState(false);
+
+  const [selectedOption, setSelectedOption] = useState("");
+  const options = [
+    { value: "option1", label: "Friday" },
+    { value: "option2", label: "Saturday" },
+    { value: "option3", label: "Sunday" },
+    // Add more options as needed
+  ];
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value); // Update the selected option in state
+  };
 
   return (
     <div className="main-container">
@@ -88,80 +99,126 @@ const ScheduleCasting = () => {
           <img className="line-body" src={LineBody} />
         </div>
 
+        <div className="day-div">
+          <p className="day">Day</p>
+          <div>
+            <select
+              className="custom-dropdown"
+              value={selectedOption}
+              onChange={handleOptionChange}
+            >
+              <option className="val" value="Thursday">
+                Thursday
+              </option>
+              {options.map((option, index) => (
+                <option key={index} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
+            {/* {selectedOption && <p>Selected option: {selectedOption}</p>} Show the selected option */}
+          </div>
+        </div>
+
         <div className="time-div">
           <p className="time-heading">Start Time</p>
           <div className="time-subdiv">
             <div className="time-subdiv-info">
-                <input className="time-subdiv-info-input" type="text" placeholder="12" />
-                <img  className="time-subdiv-info-image" src={Frame} alt="frame"/>
+              <input
+                className="time-subdiv-info-input"
+                type="text"
+                placeholder="12"
+              />
+              <img className="time-subdiv-info-image" src={Frame} alt="frame" />
             </div>
             <div className="time-subdiv-info">
-                <input className="time-subdiv-info-input" type="text" placeholder="00" />
-                <img  className="time-subdiv-info-image" src={Frame} alt="frame"/>
+              <input
+                className="time-subdiv-info-input"
+                type="text"
+                placeholder="00"
+              />
+              <img className="time-subdiv-info-image" src={Frame} alt="frame" />
             </div>
             <div className="time-subdiv-info">
-                <input className="time-subdiv-info-input" type="text" placeholder="AM" />
-                <img  className="time-subdiv-info-image" src={Frame} alt="frame"/>
+              <input
+                className="time-subdiv-info-input"
+                type="text"
+                placeholder="AM"
+              />
+              <img className="time-subdiv-info-image" src={Frame} alt="frame" />
             </div>
           </div>
 
           <p className="time-heading">End Time (Optional)</p>
           <div className="time-subdiv">
-          <div className="time-subdiv-info">
-                <input className="time-subdiv-info-input" type="text" placeholder="12" />
-                <img  className="time-subdiv-info-image" src={Frame} alt="frame"/>
+            <div className="time-subdiv-info">
+              <input
+                className="time-subdiv-info-input"
+                type="text"
+                placeholder="12"
+              />
+              <img className="time-subdiv-info-image" src={Frame} alt="frame" />
             </div>
             <div className="time-subdiv-info">
-                <input className="time-subdiv-info-input" type="text" placeholder="00" />
-                <img  className="time-subdiv-info-image" src={Frame} alt="frame"/>
+              <input
+                className="time-subdiv-info-input"
+                type="text"
+                placeholder="00"
+              />
+              <img className="time-subdiv-info-image" src={Frame} alt="frame" />
             </div>
             <div className="time-subdiv-info">
-                <input className="time-subdiv-info-input" type="text" placeholder="AM" />
-                <img  className="time-subdiv-info-image" src={Frame} alt="frame"/>
+              <input
+                className="time-subdiv-info-input"
+                type="text"
+                placeholder="AM"
+              />
+              <img className="time-subdiv-info-image" src={Frame} alt="frame" />
             </div>
           </div>
 
-          <div className="lower-body-text-div"  onClick={() => setAddTimeSlot(true)}>
+          {/* <div className="lower-body-text-div"  onClick={() => setAddTimeSlot(true)}>
             <img className="lower-body-img" src={Add} />
             <p className="lower-body-text">Add another time slot</p>
-          </div>
+          </div> */}
         </div>
       </div>
 
       <div className="casting-footer">
         <div className="casting-footer-discard">
-        <img className ="casting-footer-discard-img" src={Remove} />
-        <button className="discard-btn">DISCARD</button>
+          <img className="casting-footer-discard-img" src={Remove} />
+          <button className="discard-btn">DISCARD</button>
         </div>
         <button className="schedule-btn">SCHEDULE</button>
       </div>
 
-      <div className="wrap-up-background-image" style={
-          addTimeSlot
-            ? { transform: "translateY(0%)" }
-            : { transform: "translateY(102%)" }
-        }>
       <div
-        className="time-slot-popup"
+        className="wrap-up-background-image"
         style={
           addTimeSlot
             ? { transform: "translateY(0%)" }
             : { transform: "translateY(102%)" }
         }
-       
       >
-        <div className="time-slot-popup-options">
-          <p className="time-slot-popup-options-para">Weekdays(Mon-Fri)</p>
-        </div>
-        <div className="time-slot-popup-options">
-          <p className="time-slot-popup-options-para">Weekends(Sat-Sun)</p>
-        </div>
-        <div className="time-slot-popup-options">
-          <p className="time-slot-popup-options-para">Custom</p>
+        <div
+          className="time-slot-popup"
+          style={
+            addTimeSlot
+              ? { transform: "translateY(0%)" }
+              : { transform: "translateY(102%)" }
+          }
+        >
+          <div className="time-slot-popup-options">
+            <p className="time-slot-popup-options-para">Weekdays(Mon-Fri)</p>
+          </div>
+          <div className="time-slot-popup-options">
+            <p className="time-slot-popup-options-para">Weekends(Sat-Sun)</p>
+          </div>
+          <div className="time-slot-popup-options">
+            <p className="time-slot-popup-options-para">Custom</p>
+          </div>
         </div>
       </div>
-      </div>
-      
     </div>
   );
 };
